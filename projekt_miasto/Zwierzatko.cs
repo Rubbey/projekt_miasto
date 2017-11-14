62c760d6c463b4d54012efd572ed55adb9d1b68e
Patryk <Patryk@JEFFI2>
2017-11-14 22:30:24 +0100

    Poprawiłem i przeciążyłem metodę UstawDane w klasie Zwierzatko.

--- projekt_miasto/Zwierzatko.cs
+++ projekt_miasto/Zwierzatko.cs
@@ -18,29 +18,42 @@ abstract class Zwierzatko : IInformacje
 
         public string Imie { get; private set; }
         public int Wiek { get; private set; }
-        public RodzajZwierzatka Plec { get; private set; }
+        public RodzajZwierzatka Rodzaj { get; private set; }
 
         public Zwierzatko()     //tutaj powinny znaleźć sie odpowiednie konstruktory
         {
-            this.Plec = RodzajZwierzatka.BRAK_DANYCH;
+            this.Rodzaj = RodzajZwierzatka.BRAK_DANYCH;
             this.UstawDane(Toolbox.BRAK_DANYCH, 0);
         }
 
         public Zwierzatko(string imie, int wiek) : this()
         {
             this.UstawDane(imie, wiek);
-           
         }
 
-                                                         
-        public void UstawDane(string imie, int wiek)       //metoda UstawDane powinna być przeciążona dla kilku wariantów argumentów
-        {                                                 //dodatkowo powinna zawierać sprawdzenia poprawności wprowadzanych danych
-            if (!string.IsNullOrEmpty(imie))
-                this.Imie = imie;
-
-            Toolbox.inputInteger(wiek, 0, 99);         //Użyć metod z Toolbox do sprawdzania poprawaności !!!
-                this.Wiek = wiek;
+        public void UstawDane()                             
+        {
+            this.Imie = Toolbox.inputString("Podaj imie: ", false);
+            this.Wiek = Toolbox.inputInteger("Podaj wiek: ", 0, 99);
         }
+
+        public void UstawDane(string imie)
+            {
+                if (!string.IsNullOrEmpty(imie)) this.Imie = imie;
+                else this.Imie = Toolbox.inputString("Podaj imie: ", false);
+
+                this.Wiek = Toolbox.inputInteger("Podaj wiek: ", 0, 99);
+            }
+
+        public void UstawDane(string imie, int wiek)                             //metoda UstawDane powinna być przeciążona dla kilku wariantów argumentów
+                                                                                //dodatkowo powinna zawierać sprawdzenia poprawności wprowadzanych danych
+            {                                                                   
+                if (!string.IsNullOrEmpty(imie)) this.Imie = imie;              
+                else this.Imie = Toolbox.inputString("Podaj imie: ", false);    
+
+                if (wiek >= 0 && wiek <= 99) this.Wiek = wiek;                  
+                else this.Wiek = Toolbox.inputInteger("Podaj wiek: ", 0, 99);   
+            }
 
         /*
             metody abstrakcyjne nie posiadają w klasie , w której zostały zdefioniowane swoich deklaracji - jedynie nagłówek
@@ -53,7 +66,7 @@ abstract class Zwierzatko : IInformacje
 
         public virtual void WyswietlInformacje()
         {
-            Console.WriteLine("Imię: {0}\nWiek: {1}\nRodzaj: {3}\nGatunek: {4}\nRasa: {5}", this.Imie, this.Wiek, this.Plec);
+            Console.WriteLine("Imię: {0}\nWiek: {1}\nRodzaj: {2}\nGatunek: {3}\nRasa: {4}", this.Imie, this.Wiek, this.Rodzaj);
         }
         //ta metoda moze wyswietlic podstawowe informacje o zwierzaku
     }
