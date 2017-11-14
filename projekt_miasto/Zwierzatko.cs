7a3ced8982f3583cfdc52f2a7b30eac9cfbb2513
Janusz Strycharczyk <jachu85@gmail.com>
2017-11-14 23:33:20 +0100

    Poprawione UstawDane w Zwierzatko

--- projekt_miasto/Zwierzatko.cs
+++ projekt_miasto/Zwierzatko.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace projekt_miasto
 {
 
-abstract class Zwierzatko : IInformacje
+    abstract class Zwierzatko : IInformacje
     {
         public enum RodzajZwierzatka
         {
@@ -31,20 +31,26 @@ abstract class Zwierzatko : IInformacje
             this.UstawDane(imie, wiek);
         }
 
-        public void UstawDane()                             
+        public void UstawDane()
         {
             this.Imie = Toolbox.inputString("Podaj imie: ", false);
             this.Wiek = Toolbox.inputInteger("Podaj wiek: ", 0, 99);
         }
 
-                                                         
-        public void UstawDane(string imie, int wiek)       //metoda UstawDane powinna być przeciążona dla kilku wariantów argumentów
-        {                                                 //dodatkowo powinna zawierać sprawdzenia poprawności wprowadzanych danych
-            if (!string.IsNullOrEmpty(imie))
-                this.Imie = imie;
+        public void UstawDane(string imie)
+        {
+            if (!string.IsNullOrEmpty(imie)) this.Imie = imie;
+            else this.Imie = Toolbox.inputString("Podaj imie: ", false);            
+        }
 
-            Toolbox.inputInteger(wiek, 0, 99);         //Użyć metod z Toolbox do sprawdzania poprawaności !!!
-                this.Wiek = wiek;
+        public void UstawDane(string imie, int wiek)                             //metoda UstawDane powinna być przeciążona dla kilku wariantów argumentów
+                                                                                 //dodatkowo powinna zawierać sprawdzenia poprawności wprowadzanych danych
+        {
+            if (!string.IsNullOrEmpty(imie)) this.Imie = imie;
+            else this.Imie = Toolbox.inputString("Podaj imie: ", false);
+
+            if (wiek >= 0 && wiek <= 99) this.Wiek = wiek;
+            else this.Wiek = Toolbox.inputInteger("Podaj wiek: ", 0, 99);
         }
 
         /*
